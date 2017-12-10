49968da42c3cbf015037b41bb9104ab225c14de7
Norbert Babély <caamarth42@gmail.com>
2017-12-10 13:14:13 +0100

    setting up packages and dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import { NgModule } from '@angular/core';
+
+import { UsersModule } from './users/users.module'
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    UsersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
